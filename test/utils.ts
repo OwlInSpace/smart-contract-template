768c08a3ff46e0981096e45dcf66fe616ceefddc
Owl In Space <47359751+OwlInSpace@users.noreply.github.com>
2021-08-15 20:03:14 +0300

    Add utils for test scripts

---
+++ test/utils.ts
@@ -0,0 +1,8 @@
+export async function shouldRevert(func: () => void, errorMessage: string) {
+    try {
+        await func()
+    } catch {
+        return
+    }
+    throw new Error(errorMessage)
+}
