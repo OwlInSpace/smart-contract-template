c9726994763d582474636466f4f3f9f79f126c0b
Owl In Space <47359751+OwlInSpace@users.noreply.github.com>
2021-08-27 10:41:03 +0300

    Add constructor for starting num

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 contract Contract {
     uint256 private num;
 
+    constructor (uint256 startingNum) {
+        num = startingNum;
+    }
+
     function get() external view returns (uint256) {
         return num;
     }
